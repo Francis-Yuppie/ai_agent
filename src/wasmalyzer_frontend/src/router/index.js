f3f301594e5dd60e7bb910986ef675819c4b3533
Itachi <bscs157j2022@students.tum.ac.ke>
2025-04-02 18:00:03 +0300

    added routes and route guards

--- src/wasmalyzer_frontend/src/router/index.js
+++ src/wasmalyzer_frontend/src/router/index.js
@@ -4,25 +4,59 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: LandingView,
-    //   meta: { title: 'Omniface - Home' },
-    // },
-    // {
-    //   path: '/view-demo/:id',
-    //   name: 'view',
-    //   component: View,
-    //   meta: { title: 'Omniface - Home' },
-    //   props: true
-    // },
+    //login
+    {
+      path: '/login',
+      name: 'Login',
+      component: () => import('@/views/Login.vue'),
+      meta: { requiresAuth: false },
+    },
+    {
+      path: '/dashboard',
+      name: 'Dashboard',
+      component: () => import('@/views/Dashboard.vue'),
+      meta: { requiresAuth: true },
+    },
+    {
+      path: '/canisters',
+      name: 'Canisters',
+      component: () => import('@/views/Canisters.vue'),
+      meta: { requiresAuth: true },
+    },
+    {
+      path: '/threats',
+      name: 'Threats',
+      component: () => import('@/views/Threats.vue'),
+      meta: { requiresAuth: true },
+    },
+    {
+      path: '/reports',
+      name: 'Reports',
+      component: () => import('@/views/Reports.vue'),
+      meta: { requiresAuth: true },
+    },
+    {
+      path: '/account',
+      name: 'Account',
+      component: () => import('@/views/Account.vue'),
+      meta: { requiresAuth: true },
+    },
   ],
 })
 
-// router.beforeEach((to, from, next) => {
-// document.title = to.meta.title || 'Omniface';
-//   authMiddleware(to, from, next);
-// });
+//route guards
+router.beforeEach((to, from, next) => {
+  const isAuthenticated = localStorage.getItem('isAuthenticated')
+
+  if (to.meta.requiresAuth && !isAuthenticated) {
+    next({ name: 'Login' })
+  } else if (to.name === 'Login' && isAuthenticated) {
+    next({ name: 'Dashboard' })
+  } else {
+    next()
+  }
+})
 
 export default router
+
+
