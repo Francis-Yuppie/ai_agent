8cb570e05edfdcee1cb4fc35c512b1110f26d2a6
Francis-Yuppie <dcs00922022@students.tum.ac.ke>
2025-04-03 16:34:57 +0300

    merging

--- src/wasmalyzer_backend/src/lib.rs
+++ src/wasmalyzer_backend/src/lib.rs
@@ -4,7 +4,7 @@ use ic_cdk::api::management_canister::http_request::HttpResponse;
 mod canister_info;
 mod canister_manager;
 mod github_manager;
-mod ai_agent;
+// mod ai_agent;
 
 pub use canister_manager::{
     delete_canister, list_canisters, register_canister, update_canister, view_canister,
@@ -15,6 +15,6 @@ pub use canister_info::{canister_info, canister_metadata, canister_transactions,
 
 pub use github_manager::{link_repository,  analyze_repository, RepoAnalysisResult, Vulnerability, CodeQualityMetrics};
 
-pub use ai_agent::analyze_github_repo;
+// pub use ai_agent::analyze_github_repo;
 
 ic_cdk::export_candid!();
